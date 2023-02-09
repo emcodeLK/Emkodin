7d40a2c3ea91c58ae5b23ba7d8b3564d1973f5af
Emal <emalyaditha@gmail.com>
2023-02-10 00:54:04 +0530

    Change Background Nav Bar Update Footer Update Links added.

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,13 +1,145 @@
-<link rel="stylesheet" href="../CSS/footer.css">
-<link rel="stylesheet" href="../CSS/">
-<footer>
-	<div>
-		<H1 style="text-align:center; color:white; ">Emkodin</H1>
-		<p style="color:white;">Copyright © 2023 Emkodin</p>
-		<ul class="ftco-footer-social p-0">
-              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="fa fa-twitter"></span></a></li>
-              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="fa fa-facebook"></span></a></li>
-              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="fa fa-instagram"></span></a></li>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style media="screen">
+      *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    background: #fcfcfc;
+    font-family: sans-serif;
+}
+footer{
+    position: auto;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #111;
+    height: auto;
+    width: 100vw;
+    padding-top: 40px;
+    color: #fff;
+}
+.footer-content{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+}
+.footer-content h3{
+    font-size: 2.1rem;
+    font-weight: 500;
+    text-transform: capitalize;
+    line-height: 3rem;
+}
+.footer-content p{
+    max-width: 500px;
+    margin: 10px auto;
+    line-height: 28px;
+    font-size: 14px;
+    color: #cacdd2;
+}
+.socials{
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem 0 3rem 0;
+}
+.socials li{
+    margin: 0 10px;
+}
+.socials a{
+    text-decoration: none;
+    color: #fff;
+    border: 1.1px solid white;
+    padding: 5px;
+
+    border-radius: 50%;
+
+}
+.socials a i{
+    font-size: 1.1rem;
+    width: 20px;
+
+
+    transition: color .4s ease;
+
+}
+.socials a:hover i{
+    color: aqua;
+}
+
+.footer-bottom{
+    background: #000;
+    width: 100vw;
+    padding: 20px;
+padding-bottom: 40px;
+    text-align: center;
+}
+.footer-bottom p{
+float: left;
+    font-size: 14px;
+    word-spacing: 2px;
+    text-transform: capitalize;
+}
+.footer-bottom p a{
+  color:#44bae8;
+  font-size: 16px;
+  text-decoration: none;
+}
+.footer-bottom span{
+    text-transform: uppercase;
+    opacity: .4;
+    font-weight: 200;
+}
+.footer-menu{
+  float: right;
+
+}
+.footer-menu ul{
+  display: flex;
+}
+.footer-menu ul li{
+padding-right: 10px;
+display: block;
+}
+.footer-menu ul li a{
+  color: #cfd2d6;
+  text-decoration: none;
+}
+.footer-menu ul li a:hover{
+  color: #27bcda;
+}
+
+@media (max-width:500px) {
+.footer-menu ul{
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+}
+    </style>
+</head>
+<body>
+    <footer>
+        <div class="footer-content">
+            <h3 style="padding-left: 50px;">Emkodin</h3>
+            <p style="padding-left: 50px;">A place where
+			Software comes to life.<br>We do any kind of Software Project/Assignments.</p>
+            <ul class="socials" style="padding-left: 50px;">
+                <li><a href="https://www.facebook.com/emkodin/"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://www.instagram.com/emkodin/"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://twitter.com/emkodin"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="mailto:emkodin@gmail.com"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="https://www.tiktok.com/@emkodin?is_from_webapp=1&sender_device=pc"><i class="fa fa-tiktok"></i></a></li>
+                <!-- <li><a href="#"><i class="fa fa-youtube"></i></a></li> -->
+                <!-- <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li> -->
             </ul>
-	</div>
-</footer>
+        </div>
+        <div class="footer-bottom">
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Copyright &copy; 2023 <a href="#">Emkodin</a></p>
+        </div>
+
+    </footer>
